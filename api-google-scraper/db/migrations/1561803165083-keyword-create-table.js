d9f077a7ad8205c8e4f36a566982f2b23f0d3d0b
Robert Neal <rmn316@gmail.com>
2019-06-29 19:03:08 +0100

    #15 Created entity and migration for keyword table.

---
+++ api-google-scraper/db/migrations/1561803165083-keyword-create-table.js
@@ -0,0 +1,49 @@
+/**
+ * Create user table migration
+ * @param {object} queryInterface
+ * @param {Sequelize} Sequelize
+ * @returns {*}
+ */
+export const up = (queryInterface, Sequelize) => {
+  return queryInterface.createTable('Keyword', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    title: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    adWords: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+    links: {
+      allowNull: false,
+      type: Sequelize.INTERGER,
+      defaultValue: 0,
+    },
+    totalResults: {
+      allowNull: false,
+      type: Sequelize.BIGINT,
+      defaultValue: 0,
+    },
+    source: {
+      allowNull: false,
+      type: Sequelize.TEXT,
+    },
+  });
+};
+
+/**
+ * Rollback create user table migration
+ * @param {object} queryInterface
+ * @param {Sequelize} Sequelize
+ * @returns {*}
+ */
+export const down = (queryInterface, Sequelize) => {
+  return queryInterface.dropTable('Keyword');
+};
---
+++ api-google-scraper/src/models/keyword.js
@@ -0,0 +1,35 @@
+export default function(sequelize, DataType) {
+  const Keyword = sequelize.define('User', {
+    id: {
+      type: DataType.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    adWords: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    links: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    totalResults: {
+      type: DataType.BIGINT,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    source: {
+      type: DataType.TEXT,
+      allowNull: false,
+    }
+  }, {
+    tableName: 'keywords',
+  });
+  return Keyword;
+}
