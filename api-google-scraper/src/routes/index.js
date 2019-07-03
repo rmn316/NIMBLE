d79b10495984dad5d5f446168359d75f8975d24c
Robert Neal <rmn316@gmail.com>
2019-07-03 09:48:11 +0100

    #17 added auth middleware to routes.
    # Conflicts:
    #	api-google-scraper/src/routes/index.js

--- api-google-scraper/src/routes/index.js
+++ api-google-scraper/src/routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import KeywordsController from '../controllers/keywords.controller';
 import UsersController from '../controllers/users.controller';
+import authMiddleware from '../middleware/auth';
 import multer from 'multer';
 
 const upload = multer({ dest: 'tmp/csv/' });
@@ -8,7 +9,7 @@ const upload = multer({ dest: 'tmp/csv/' });
 const router = Router();
 
 router.post('/users/login', UsersController.login);
-router.get('/keywords', KeywordsController.index);
-router.post('/keywords/upload', upload.single('file'), KeywordsController.upload);
+router.get('/keywords', authMiddleware.verifyToken, KeywordsController.index);
+router.post('/keywords/upload', [upload.single('file'), authMiddleware.verifyToken], KeywordsController.upload);
 
 export default router;
